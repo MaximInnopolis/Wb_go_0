f30bd8395d980846c54097e758b7663f66bad9aa
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-08-20 15:59:40 +0300

    Divided model into submodels

---
+++ internal/models/delivery.go
@@ -0,0 +1,12 @@
+package models
+
+type Delivery struct {
+	OrderId int    `gorm:"column:order_id"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region"`
+	Email   string `json:"email"`
+}
